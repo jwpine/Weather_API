927eee875bf11b184465f770aa26b30bbb92a797
John Pine <75548572+jwpine@users.noreply.github.com>
2022-08-10 16:53:44 -0400

    Update weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -1,12 +1,14 @@
 import requests
 import time
 import openpyxl
+from bs4 import BeautifulSoup
 
 # Get Current Time and Date
 date = time.strftime("%m/%d/%Y")
 clock = time.strftime("%H:%M:%S")
 
-# Storage for Staion List
+#********************************** API RESULTS **********************************#
+# Storage for Station List
 chicago_station = 'KMDW'
 ny_station = 'KNYC'
 
@@ -21,13 +23,38 @@ ny_results = requests.get(ny_url).json()
 # Get the Current Temperature
 chicago_temp_c = chicago_results['features'][0]['properties']['temperature']['value']
 ny_temp_c = ny_results['features'][0]['properties']['temperature']['value']
+print(f'chicago c {chicago_temp_c}')
+print(f'nyc c {ny_temp_c}')
 
 # Convert C to F
 chicago_temp_f = (chicago_temp_c * (9/5)) + 32
 ny_temp_f = (ny_temp_c * (9/5)) + 32
+print(f'chicago f {chicago_temp_f}')
+print(f'nyc f {ny_temp_f}')
 
-# Open an Excel Workbook to add the Data
-book = openpyxl.load_workbook('Weather_Data.xlsx')
+#********************************** Website Results **********************************#
+# Store the URLs
+chicago_web_url = 'https://forecast.weather.gov/MapClick.php?lat=41.7881&lon=-87.7421'
+ny_web_url = 'https://forecast.weather.gov/MapClick.php?lat=40.7823&lon=-73.9654'
+
+# Use Requests to Get the Websites
+chicago_web_results = requests.get(chicago_web_url).text
+ny_web_results = requests.get(ny_web_url).text
+
+# Soup the results and Parse
+chicago_soup = BeautifulSoup(chicago_web_results, 'html.parser')
+ny_soup = BeautifulSoup(ny_web_results, 'html.parser')
+
+# Find the paragraph with the data we want
+#chicago_div = chicago_soup.find('div', id='current_conditions-summary')
+chicago_web_temp = chicago_soup.find('p', class_='myforecast-current-lrg')
+ny_web_temp = ny_soup.find('p', class_='myforecast-current-lrg')
+
+
+#********************************** Excel Results **********************************#
+
+# Open an Excel Workbook to add the data
+book = openpyxl.load_workbook('Temp_Data.xlsx')
 sheet = book.active
 
 # Insert a Row at the top so it is sorted properly
@@ -38,6 +65,7 @@ date_excel = sheet.cell(row=2, column=1)
 date_excel.value = date
 time_excel = sheet.cell(row=2, column=2)
 time_excel.value = clock
+# API RESULTS
 chicago_f_excel = sheet.cell(row=2, column=3)
 chicago_f_excel.value = chicago_temp_f
 chicago_c_excel = sheet.cell(row=2, column=4)
@@ -46,6 +74,11 @@ ny_f_excel = sheet.cell(row=2, column=5)
 ny_f_excel.value = ny_temp_f
 ny_c_excel = sheet.cell(row=2, column=6)
 ny_c_excel.value = ny_temp_c
+# Web RESULTS
+chicago_web_excel = sheet.cell(row=2, column=7)
+chicago_web_excel.value = chicago_web_temp.text
+ny_web_excel = sheet.cell(row=2, column=8)
+ny_web_excel.value = ny_web_temp.text
 
 # Save the Workbook
-book.save('Weather_Data.xlsx')
+book.save('Temp_Data.xlsx')
